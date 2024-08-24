f66894f3b44b8d826cec40db3ef192de1963beee
sushal07 <142735020+sushal07@users.noreply.github.com>
2024-08-24 12:30:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-VANTA.NET({
-  el: "#net",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0x3fe8ff,
-  backgroundColor: 0xf2a52
-})
+
